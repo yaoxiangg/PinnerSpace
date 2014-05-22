1e86d16d4c00412facfd26fec0baaaaa8acac356
yaoxiangg <teryaoxiang@gmail.com>
2014-05-22 11:21:35 +0800

    Readded Nickname + Settings page
    Reconfigured home button to redirect to index.html
    Readded nickname to the account (Default uses email header as nickname)
    Displays nickname now

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ jinja_environment = jinja2.Environment(
 class Account(ndb.Model):
 	#email is key, refer using id
 	accid = ndb.StringProperty()
+	usernick = ndb.StringProperty()
 
 #Item Datastore - Child of Board
 class Item(ndb.Model):
@@ -60,20 +61,42 @@ def loadBoard(user, self):
 	if userKey == None:
 		#Create Acc
 		logging.debug("Creating Account for: " + user.email())
-		currUser = Account(id=user.email(), accid=user.user_id())
-		currkey = currUser.put()
+		currUser = Account(id=user.email(), accid=user.user_id(), usernick=user.nickname())
+		userKey = currUser.put()
 
 	#Logging into Board - LoadBoard
 	logging.debug("Logging in to: " + user.email())
 	parameters = {
 	'user_mail': user.email(),
-	'user_nick': user.nickname(),
+	'user_nick': userKey.usernick,
 	'logout': users.create_logout_url(self.request.host_url),
 	}
 	webpage = jinja_environment.get_template('index.html')
 	self.response.out.write(webpage.render(parameters))
 
+#Settings page - Change nickname
+class updateProfile(webapp2.RequestHandler):
+	def get(self):
+		userKey = ndb.Key('Account', users.get_current_user().email()).get()
+		if userKey:
+			parameters = {
+			'user_mail': users.get_current_user().email(),
+			'user_nick': userKey.usernick,
+			'logout': users.create_logout_url(self.request.host_url),
+			}
+			webpage = jinja_environment.get_template('setting.html')
+			self.response.out.write(webpage.render(parameters))
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+
+	def post(self):
+		userKey = ndb.Key('Account', users.get_current_user().email()).get()
+		userKey.usernick = self.request.get('newnickname')
+		userKey.put()
+		self.redirect(self.request.uri)
 
 #App
 app = webapp2.WSGIApplication([('/', MainHandler),
-	('/board', ShowBoard)], debug=True)
+	('/board', ShowBoard),
+	('/settings', updateProfile),
+	('/update', updateProfile)], debug=True)
